1a7f75c79db7cf9a3e9b6ae133f785e968bbecaf
AndyOoh <andy.oeee@gmail.com>
2022-12-14 19:46:28 +0700

    dApp and hardhat are projects are now siblings. auto-update frontend constants works

---
+++ hardhat/utils/helper-hardhat-config.ts
@@ -0,0 +1,3 @@
+const frontEndContractsFiles =
+  '../nextjs-smartcontract-lottery-fcc/constants/contractAddresses.json';
+const frontEndAbiFiles = '../nextjs-smartcontract-lottery-fcc/constants/abi.json';
