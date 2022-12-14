3f0d7c01aca537fdecf3a422bb65d00a3ea51525
AndyOoh <andy.oeee@gmail.com>
2022-12-15 01:06:02 +0700

    delete unused files

--- hardhat/utils/helper-hardhat-config.ts
+++
@@ -1,3 +0,0 @@
-const frontEndContractsFiles =
-  '../nextjs-smartcontract-lottery-fcc/constants/contractAddresses.json';
-const frontEndAbiFiles = '../nextjs-smartcontract-lottery-fcc/constants/abi.json';
