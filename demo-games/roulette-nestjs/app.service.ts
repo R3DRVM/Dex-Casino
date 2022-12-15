714ccf2623aedae661cde1ff375892f6818eaae7
R3DRVM <99007375+R3DRVM@users.noreply.github.com>
2022-12-14 17:22:06 -0800

    Update app.service.ts

--- demo-games/roulette-nestjs/app.service.ts
+++ demo-games/roulette-nestjs/app.service.ts
@@ -5,4 +5,16 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  playRoulette(number: number): string {
+    // Generate a random number to determine the outcome of the game
+    const outcome = Math.floor(Math.random() * 36);
+  
+    // Check if the player won or lost the bet
+    if (number === outcome) {
+      return 'You won!';
+    } else {
+      return 'You lost.';
+    }
+  }
 }
